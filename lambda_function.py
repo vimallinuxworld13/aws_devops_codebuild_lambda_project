125d3fd1c3b154026c3fed6857cdd88ae12ec312
Vimal Daga <vdaga@lwindia.com>
2024-09-09 22:31:11 +0530

    final

--- lambda_function.py
+++ lambda_function.py
@@ -4,10 +4,10 @@ import os
 
 def lw(event, context):
     
-    print("i m Vimal Daga from my World i m dev  ")
+    print("i m Vimal Daga from my World i m dev  final test ")
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! from LW by VD  dev dev ')
+        'body': json.dumps('Hello from Lambda! from LW by VD  final test and bye  ')
     }
 
